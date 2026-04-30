b1331d1d620f04607241ed7d5d34f052608ebeb5
NBnY5A <victor.lito@a.ucb.br>
2026-04-30 16:22:57 -0300

    feat: Update WalletDataAlreadyExistsException

--- src/main/java/com/victor/jbank/exception/WalletDataAlreadyExistsException.java
+++ src/main/java/com/victor/jbank/exception/WalletDataAlreadyExistsException.java
@@ -1,7 +1,23 @@
 package com.victor.jbank.exception;
 
-public class WalletDataAlreadyExistsException extends JBankException{
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+
+public class WalletDataAlreadyExistsException extends JBankException {
+    private final String message;
+
     public WalletDataAlreadyExistsException(String message) {
         super(message);
+        this.message = message;
+    }
+
+    @Override
+    public ProblemDetail toProblemDetail() {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatusCode.valueOf(442));
+
+        problemDetail.setTitle("Wallet data already exists");
+        problemDetail.setDetail(message);
+
+        return problemDetail;
     }
 }
