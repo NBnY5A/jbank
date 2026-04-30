3551661c2a802beb64950cde6419b98ef15a1c14
NBnY5A <victor.lito@a.ucb.br>
2026-04-30 16:23:22 -0300

    feat: Update CreateWalletDTO to validate fields before process request

--- src/main/java/com/victor/jbank/controllers/dto/CreateWalletDTO.java
+++ src/main/java/com/victor/jbank/controllers/dto/CreateWalletDTO.java
@@ -1,8 +1,19 @@
 package com.victor.jbank.controllers.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.br.CPF;
+
 public record CreateWalletDTO(
+        @NotBlank
         String name,
+
+        @CPF
+        @NotBlank
         String cpf,
+
+        @Email
+        @NotBlank
         String email
 ) {
 }
