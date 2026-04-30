0ecb77bd32862a4bf102c55e8671d8ca02d2fcc6
NBnY5A <victor.lito@a.ucb.br>
2026-04-30 16:22:46 -0300

    feat: Update JBankException to be the default exception

--- src/main/java/com/victor/jbank/exception/JBankException.java
+++ src/main/java/com/victor/jbank/exception/JBankException.java
@@ -1,5 +1,8 @@
 package com.victor.jbank.exception;
 
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+
 public abstract class JBankException extends RuntimeException {
     public JBankException(String message) {
         super(message);
@@ -7,5 +10,14 @@ public abstract class JBankException extends RuntimeException {
 
     public JBankException(Throwable cause) {
         super(cause);
+    }
+
+    public ProblemDetail toProblemDetail() {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatusCode.valueOf(442));
+
+        problemDetail.setTitle("JBank Internal Server Error");
+        problemDetail.setDetail("Contact JBank Support");
+
+        return problemDetail;
     }
 }
